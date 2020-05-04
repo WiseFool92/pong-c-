1dcf93a83cbe86f1954060e694769381af91a1a1
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-05-04 11:09:37 -0700

    ping-pong # printing program

--- pong.cs
+++ pong.cs
@@ -6,13 +6,29 @@ class Pong
   {
     Console.WriteLine("Choose a #");
     string numberInput = Console.ReadLine();
-    int number = int.Parse(numberInput);
+    int number = Int32.Parse(numberInput);
 
-    for (int index = 0; index < number; index++)
+    for (int index = 0; index <= number; index++)
     {
-      if (index % 3 == 0 && index % 5 == 0 )
+      if (index == 0)
+      {
+        Console.WriteLine(index);
+      }
+      else if (index % 3 == 0 && index % 5 == 0 )
       {
         Console.WriteLine("ping-pong");
+      } 
+      else if (index % 3 == 0)
+      {
+        Console.WriteLine("ping");
+      }
+      else if (index % 5 == 0)
+      {
+        Console.WriteLine("pong");
+      }
+      else
+      {
+        Console.WriteLine(index);
       }
     }
   }
